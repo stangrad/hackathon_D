570413a34dbe5f8044de84f87147d730d25b4b3d
730693 <730693@elf8it221376.ACDM.DS.SAIT.CA>
2019-02-16 15:49:00 -0700

    3nd_Tina Wang - the class implement  the user functions

---
+++ hackathon_D/src/user/User.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package user;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * @author yunting Wang
+ *
+ */
+public class User {
+	public User() {
+		ArrayList <UserList> userList = new ArrayList <>();
+		Scanner kb = new Scanner(System.in);
+	}
+}
