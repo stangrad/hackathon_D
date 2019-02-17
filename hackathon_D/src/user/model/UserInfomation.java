6b6188c9f927b570885fd17d36e1ac50ad2dc54b
730693 <730693@elf8it221376.ACDM.DS.SAIT.CA>
2019-02-16 17:32:48 -0700

    Tina  common out

--- hackathon_D/src/user/model/UserInfomation.java
+++ hackathon_D/src/user/model/UserInfomation.java
@@ -21,25 +21,25 @@ public class UserInfomation {
 	public UserInfomation() throws IOException {
 		//loadData(dataPath);
 		if(loing()) {
-			System.out.println("Login Success");
-			System.out.println("Welcome to Initial Power.");
+			//System.out.println("Login Success");
+			//System.out.println("Welcome to Initial Power.");
 		}else{
-			System.out.println("Sorry, id or password is incorrect. Please check it.");
-			System.out.println("If you are not our customer, welcome to join Initial Power for createing your income.");
+			//System.out.println("Sorry, id or password is incorrect. Please check it.");
+			//System.out.println("If you are not our customer, welcome to join Initial Power for createing your income.");
 		}
 	}
 	private boolean loing() throws IOException {
 		//get userID;
-		System.out.println("Typing userID:");
+		//System.out.println("Typing userID:");
 		String id = kb.nextLine();
 		while( id.isEmpty() || !(id.matches("[0-9]{4}"))) {
-			System.out.println("Please typing 4 digits: ");
+			//System.out.println("Please typing 4 digits: ");
 			id = kb.nextLine();
 		}
-		System.out.println("Please typing pw, 6 digits:");
+		//System.out.println("Please typing pw, 6 digits:");
 		String pw = kb.nextLine();
 		while(!pw.matches("[a-zA-Z0-9]{6}")) {
-			System.out.println("Please type 6 digits(Caption, small letter, and number is mixed.");
+			//System.out.println("Please type 6 digits(Caption, small letter, and number is mixed.");
 		}		
 		if(verifyUser(id, pw)) {
 			return true;
@@ -57,8 +57,6 @@ public class UserInfomation {
 		line = bfReader.readLine();
 		while(line != null) {
 			String field [] = line.split(",");
-//			System.out.printf("[TP_1]id=%s, pw=%s\n",id,pw);
-//			System.out.printf("[TP_2]id=%s, pw=%sn",field[0], field[8]);
 			if(field[0].equals(id)) {
 				
 				if(field[8].equals(pw)) {
